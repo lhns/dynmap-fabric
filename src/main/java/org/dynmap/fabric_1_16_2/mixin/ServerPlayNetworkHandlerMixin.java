858e92f358ec7bb32f1e2bb620c6af305cbfa1a6
Pierre Kisters <pierrekisters@gmail.com>
2020-08-14 01:55:47 +0200

    fix commands

--- src/main/java/org/dynmap/fabric_1_16_2/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/org/dynmap/fabric_1_16_2/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,25 +3,29 @@ package org.dynmap.fabric_1_16_2.mixin;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.dynmap.fabric_1_16_2.context.OnGameMessageContext;
+import org.dynmap.fabric_1_16_2.event.ServerChatEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin {
     @Shadow
     public ServerPlayerEntity player;
 
-    @Inject(method = "onGameMessage", at = @At("HEAD"))
-    public void onGameMessageBegin(ChatMessageC2SPacket packet, CallbackInfo info) {
-        OnGameMessageContext.begin(this.player);
-    }
-
-    @Inject(method = "onGameMessage", at = @At("RETURN"))
-    public void onGameMessageEnd(ChatMessageC2SPacket packet, CallbackInfo info) {
-        OnGameMessageContext.end();
+    @Inject(
+            method = "onGameMessage",
+            at = @At(
+                    value = "INVOKE",
+                    target = "broadcastChatMessage",
+                    shift = At.Shift.BEFORE
+            ),
+            locals = LocalCapture.CAPTURE_FAILSOFT
+    )
+    public void onGameMessage(ChatMessageC2SPacket packet, CallbackInfo info, String string) {
+        ServerChatEvents.EVENT.invoker().onChatMessage(player, string);
     }
 }
